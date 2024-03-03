b7d958e4f3c01c35f8dfeb76ec811f0e9f468cd0
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-03 13:39:45 -0300

    deactivate anoying rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
+  ],
 };
