e7bd32ed1ab98f2cc3b03ff4481b6418e73404bb
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-06 20:01:18 -0300

    install and configure paper provider

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import {FC} from 'react';
 import {SafeAreaView} from 'react-native';
 import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
 import {CounterScreen} from './src/presentation/screens/CounterScreen';
+import {PaperProvider} from 'react-native-paper';
 
 export const App: FC = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <CounterScreen />
-      {/* <HelloWorldScreen /> */}
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={{flex: 1}}>
+        <CounterScreen />
+        {/* <HelloWorldScreen /> */}
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
