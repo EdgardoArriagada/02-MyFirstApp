39405cccbe7c3b87e0b16a6294ca15126e63bee0
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-06 20:46:41 -0300

    configure icons props like a pro

--- App.tsx
+++ App.tsx
@@ -5,9 +5,17 @@ import {CounterScreen} from './src/presentation/screens/CounterScreen';
 import {PaperProvider} from 'react-native-paper';
 import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
 
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import {IconProps} from 'react-native-paper/lib/typescript/components/MaterialCommunityIcon';
+
+const Icon: FC<IconProps> = props => <IonIcon {...props} />;
+
 export const App: FC = () => {
   return (
-    <PaperProvider>
+    <PaperProvider
+      settings={{
+        icon: Icon,
+      }}>
       <SafeAreaView style={{flex: 1}}>
         {/* <CounterScreen /> */}
         {/* <HelloWorldScreen /> */}
--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,15 +2,13 @@ import {FC, useState} from 'react';
 import {View, Text} from 'react-native';
 import {gs} from '../theme/global.styles';
 import {FAB} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 export const CounterM3Screen: FC = () => {
   const [count, setCounter] = useState(0);
   return (
     <View style={gs.centerContainer}>
       <Text style={gs.title}>{count}</Text>
-      <Icon name="accessibility-outline" size={25}  />
-      <FAB style={gs.fab} label="+1" onPress={() => setCounter(s => s + 1)} />
+      <FAB style={gs.fab} icon="add" onPress={() => setCounter(s => s + 1)} />
     </View>
   );
 };
